102eef46a07c2eb335c31b05ce47781de2f4ba56
Robin Dirksen <robin@famdirksen.nl>
2021-03-12 17:15:30 +0100

    Update CookieDurationForever.php

--- src/Duration/CookieDurationForever.php
+++ src/Duration/CookieDurationForever.php
@@ -6,7 +6,7 @@ use Famdirksen\LaravelReferral\Contracts\ReferralCookieDurationContract;
 
 class CookieDurationForever implements ReferralCookieDurationContract
 {
-    public static function getMinutesToStore(): int
+    public function getMinutesToStore(): int
     {
         // Store the cookie forever (5 years - 365 days * 5).
         return 2628000;
